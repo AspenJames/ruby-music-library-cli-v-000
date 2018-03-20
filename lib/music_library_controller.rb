60ae69d9d85e134b61797929cd6d665c1ff3fc4c
Aspen James <aspenjames@tqca.org>
2018-03-20 22:32:28 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,7 +22,7 @@ class MusicLibraryController
   def list_songs
     songs = Song.all.sort_by{|s| s.name}
     songs.each_with_index do |song, index|
-      puts "#{index + 1}. #{song.artist} - #{song.name} - #{song.genre}"
+      puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
 
